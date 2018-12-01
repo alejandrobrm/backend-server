cc4940636c378cbcd426fa84034af2ed7b972e90
Alejandro Rosales <alejandro_rosalesg@outlook.com>
2018-12-01 16:39:39 -0600

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 // Requires
 var express = require('express');
+var bodyParser = require('body-parser')
 var mongoose = require('mongoose');
 
 
 // Inicializar variables
 var app = express();
 
+ 
+// parse application/json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+
+// Importar Rutas
+
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+//Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
 
@@ -16,16 +32,6 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 });
 
-
-// Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-
-});
 
 
 // Escuchar peticiones
